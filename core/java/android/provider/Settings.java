cd13e1071fba25e7ca31682ae66d301fb2047f72
Ido Ben-Hur <idoybh2@gmail.com>
2021-11-26 15:21:20 +0200

    BrightnessMirrorController: Don't show auto brightness icon when it's disabled
    Also remove other junk from all around that commit (https://github.com/yaap/frameworks_base/commit/0da42b4c536ab82377f9b2d23b6f156daac9ca13)
    That includes but not limited to:
    * Misuse of boolean vars
    * Unused public functions
    * Annoying reversed logic
    * Redundant global vars
    * Pointless & Incorrect null checks
    
    -----
    
    SystemUI: QSAnimator: Properly animate bottom brightness slider
    
    The way it used to be the view finished animating before even
    showing. Very unpleasant and unnatural.
    
    Change-Id: I8e66ae05b1a327cd3d916cf4e51341064d3d94aa
    
    -----
    
    base: QS brightness slider settings: tunables -> Settings
    
    So we can back em up and not use tunables
    Also fix some code while at it
    
    Change-Id: I224450ba90a0dce10739f3467dd3945f9ac59fdd
    
    -----
    
    SystemUI: Correctly update brightness slider settings
    
    When adding a new brightness view
    Also some other minor cleanup
    
    Change-Id: I7b96c29ec62ad1f7705e4efb11f185f09d98af72
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12353,6 +12353,26 @@ public final class Settings {
          */
         public static final String SWAP_CAPACITIVE_KEYS = "swap_capacitive_keys";
 
+        /*
+         * Whether to show QS brightness slider
+         * @hide
+         */
+        @Readable
+        public static final String QS_SHOW_BRIGHTNESS = "qs_show_brightness";
+
+        /**
+         * Whether to show QS brightness slider below the tiles
+         * @hide
+         */
+        @Readable
+        public static final String QS_BRIGHTNESS_POSITION_BOTTOM = "qs_brightness_position_bottom";
+
+        /**
+         * Whether to show QS auto brightness toggle button
+         * @hide
+         */
+        public static final String QS_SHOW_AUTO_BRIGHTNESS_BUTTON = "qs_show_auto_brightness_button";
+
         /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
