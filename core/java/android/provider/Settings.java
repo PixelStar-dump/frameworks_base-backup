cce0b744b63de757b7b5f915f0f0d92d16d0f528
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-11-10 01:38:14 +0530

    SystemUI: Allow hiding QS footer data usage [1/2]
    Change-Id: I7ac3093c12728ac6ed63225944d8a10da2ea69ed
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6791,6 +6791,12 @@ public final class Settings {
         public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
 
         /**
+         * Whether to show daily data usage in the QS footer.
+         * @hide
+         */
+        public static final String QS_SHOW_DATA_USAGE = "qs_show_data_usage";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
