AOSP-Krypton/frameworks_base

base: use a double click effect for charging if there is no amplitude control support
Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

services/core/java/com/android/server/power/Notifier.java
@@ -108,6 +108,8 @@ public class Notifier {
     private static final VibrationEffect CHARGING_VIBRATION_EFFECT =
             VibrationEffect.createWaveform(CHARGING_VIBRATION_TIME, CHARGING_VIBRATION_AMPLITUDE,
                     -1);
+    private static final VibrationEffect CHARGING_VIBRATION_DOUBLE_CLICK_EFFECT =
+            VibrationEffect.createPredefined(VibrationEffect.EFFECT_DOUBLE_CLICK);
     private static final AudioAttributes VIBRATION_ATTRIBUTES = new AudioAttributes.Builder()
             .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
             .build();
@@ -809,7 +811,8 @@ public class Notifier {
         final boolean vibrate = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                 Settings.Secure.CHARGING_VIBRATION_ENABLED, 1, userId) != 0;
         if (vibrate) {
-            mVibrator.vibrate(CHARGING_VIBRATION_EFFECT, VIBRATION_ATTRIBUTES);
+            mVibrator.vibrate(mVibrator.hasAmplitudeControl() ? CHARGING_VIBRATION_EFFECT :
+                CHARGING_VIBRATION_DOUBLE_CLICK_EFFECT, VIBRATION_ATTRIBUTES);
         }
 
         // play sound

PixelStar-dump/frameworks_base-backup

Signed-off-by: Pranav <npv12@iitbbs.ac.in>

services/core/java/com/android/server/power/Notifier.java
@@ -119,6 +119,8 @@ public class Notifier {
                     -1);
     private static final VibrationAttributes HARDWARE_FEEDBACK_VIBRATION_ATTRIBUTES =
             VibrationAttributes.createForUsage(VibrationAttributes.USAGE_HARDWARE_FEEDBACK);
+    private static final VibrationEffect CHARGING_VIBRATION_DOUBLE_CLICK_EFFECT =
+            VibrationEffect.createPredefined(VibrationEffect.EFFECT_DOUBLE_CLICK);
 
     private final Object mLock = new Object();
 
@@ -958,8 +960,8 @@ public class Notifier {
             final boolean vibrate = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                     Settings.Secure.CHARGING_VIBRATION_ENABLED, 1, userId) != 0;
             if (vibrate) {
-                mVibrator.vibrate(CHARGING_VIBRATION_EFFECT,
-                        HARDWARE_FEEDBACK_VIBRATION_ATTRIBUTES);
+                mVibrator.vibrate(mVibrator.hasAmplitudeControl() ? CHARGING_VIBRATION_EFFECT :
+                    CHARGING_VIBRATION_DOUBLE_CLICK_EFFECT, HARDWARE_FEEDBACK_VIBRATION_ATTRIBUTES);
             }
 
             // play sound