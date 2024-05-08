LineageOS/android_frameworks_base

SettingsLib: Handle WifiService being null
During development the Wi-Fi service might not work, let's handle this
case so the Setting app doesn't crash when opening the about phone
page

Logic borrowed from the Bluetooth controller

Change-Id: Id8f6ba5d7cbac5780363612b50848ecfea10bc04

packages/SettingsLib/src/com/android/settingslib/deviceinfo/AbstractWifiMacAddressPreferenceController.java
@@ -50,16 +50,14 @@ public abstract class AbstractWifiMacAddressPreferenceController
     };
 
     private Preference mWifiMacAddress;
-    private final WifiManager mWifiManager;
 
     public AbstractWifiMacAddressPreferenceController(Context context, Lifecycle lifecycle) {
         super(context, lifecycle);
-        mWifiManager = context.getSystemService(WifiManager.class);
     }
 
     @Override
     public boolean isAvailable() {
-        return true;
+        return context.getSystemService(WifiManager.class) != null;
     }
 
     @Override
@@ -70,10 +68,8 @@ public abstract class AbstractWifiMacAddressPreferenceController
     @Override
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
-        if (isAvailable()) {
-            mWifiMacAddress = screen.findPreference(KEY_WIFI_MAC_ADDRESS);
-            updateConnectivity();
-        }
+        mWifiMacAddress = screen.findPreference(KEY_WIFI_MAC_ADDRESS);
+        updateConnectivity();
     }
 
     @Override
@@ -84,14 +80,15 @@ public abstract class AbstractWifiMacAddressPreferenceController
     @SuppressLint("HardwareIds")
     @Override
     protected void updateConnectivity() {
-        final String[] macAddresses = mWifiManager.getFactoryMacAddresses();
+        WifiManager wifiService = context.getSystemService(WifiManager.class);
+        if (wifiService == null || mWifiMacAddress == null) {
+            return;
+        }
+
+        final String[] macAddresses = wifiService.getFactoryMacAddresses();
         String macAddress = null;
         if (macAddresses != null && macAddresses.length > 0) {
             macAddress = macAddresses[0];
-        }
-
-        if (mWifiMacAddress == null) {
-            return;
         }
 
         if (TextUtils.isEmpty(macAddress) || macAddress.equals(WifiInfo.DEFAULT_MAC_ADDRESS)) {

PixelStar-dump/frameworks_base-backup

packages/SettingsLib/src/com/android/settingslib/deviceinfo/AbstractWifiMacAddressPreferenceController.java
@@ -70,10 +70,8 @@ public abstract class AbstractWifiMacAddressPreferenceController
     @Override
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
-        if (isAvailable()) {
-            mWifiMacAddress = screen.findPreference(KEY_WIFI_MAC_ADDRESS);
-            updateConnectivity();
-        }
+        mWifiMacAddress = screen.findPreference(KEY_WIFI_MAC_ADDRESS);
+        updateConnectivity();
     }
 
     @Override
@@ -84,14 +82,14 @@ public abstract class AbstractWifiMacAddressPreferenceController
     @SuppressLint("HardwareIds")
     @Override
     protected void updateConnectivity() {
+        if (mWifiManager == null || mWifiMacAddress == null) {
+            return;
+        }
+
         final String[] macAddresses = mWifiManager.getFactoryMacAddresses();
         String macAddress = null;
         if (macAddresses != null && macAddresses.length > 0) {
             macAddress = macAddresses[0];
-        }
-
-        if (mWifiMacAddress == null) {
-            return;
         }
 
         if (TextUtils.isEmpty(macAddress) || macAddress.equals(WifiInfo.DEFAULT_MAC_ADDRESS)) {