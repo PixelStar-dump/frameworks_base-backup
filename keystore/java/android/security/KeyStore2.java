RisingTechOSS/android_frameworks_base

keystore: Resolve conflicts with custom keybox
* some users might prefer using their own keybox through trickystore, add a toggle to avoid conflicts

Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

keystore/java/android/security/KeyStore2.java
@@ -24,6 +24,7 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.ServiceSpecificException;
 import android.os.StrictMode;
+import android.os.SystemProperties;
 import android.security.keymaster.KeymasterDefs;
 import android.system.keystore2.Domain;
 import android.system.keystore2.IKeystoreService;
@@ -284,9 +285,12 @@ public class KeyStore2 {
     public KeyEntryResponse getKeyEntry(@NonNull KeyDescriptor descriptor)
             throws KeyStoreException {
         StrictMode.noteDiskRead();
-
-        KeyEntryResponse response = handleRemoteExceptionWithRetry((service) -> service.getKeyEntry(descriptor));
-        return PixelPropsUtils.onGetKeyEntry(response);
+        KeyEntryResponse response = handleRemoteExceptionWithRetry(service -> service.getKeyEntry(descriptor));
+        if (SystemProperties.getBoolean("persist.sys.pixelprops.gms", true)) {
+            return PixelPropsUtils.onGetKeyEntry(response);
+        } else {
+            return response;
+        }
     }
 
     /**

PixelStar-dump/frameworks_base-backup

keystore/java/android/security/KeyStore2.java
@@ -24,6 +24,7 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.ServiceSpecificException;
 import android.os.StrictMode;
+import android.os.SystemProperties;
 import android.security.keymaster.KeymasterDefs;
 import android.system.keystore2.Domain;
 import android.system.keystore2.IKeystoreService;
@@ -285,9 +286,13 @@ public class KeyStore2 {
             throws KeyStoreException {
         StrictMode.noteDiskRead();
 
-        KeyEntryResponse response = handleRemoteExceptionWithRetry((service) -> service.getKeyEntry(descriptor));
+        KeyEntryResponse response = handleRemoteExceptionWithRetry(service -> service.getKeyEntry(descriptor));
 
-        return AttestationHooks.onGetKeyEntry(response);
+        if (SystemProperties.getBoolean("persist.sys.pixelprops.gms", true)) {
+            return AttestationHooks.onGetKeyEntry(response);
+        } else {
+            return response;
+        }
     }
 
     /**